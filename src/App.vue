6bdc9eb519cd5010222f5683e64e261e4051ac15
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2024-10-22 14:18:35 +0200

    Add ex1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script setup>
-import CompPare from './components/CompPare.vue'
+  import { ref } from 'vue'
+
+  const contactName = ref('')
+  const contactNumber = ref ('')
+
+  // Afegir contacte
+  const addContact = () => {
+    if (contactName.value && contactNumber.value) {
+      console.log('Nou contacte:', {
+        name: contactName.value,
+        number: contactNumber.value
+      })
+
+      // Limpiar el formulario
+      contactName.value = ''
+      contactNumber.value = ''
+    }
+  }
 </script>
 
 <template>
   <div>
-    <CompPare></CompPare>
+    <h1>Llibreta de Contactes</h1>
+
+    <!--formulari per afegir els contactes-->
+    <input v-model="contactName" placeholder="Escriu el nom..."/>
+    <input v-model="contactNumber" placeholder="Escriu el número de teléfon..."/>
+    <button @click="addContact">Afegir contacte</button>
+
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+  background-color: rgb(168, 233, 241);
+}
+</style>
